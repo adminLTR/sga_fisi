5ccadef8034019acf8ad2ff85bfb584a0a789415
Luis LT <105554066+LTprograms@users.noreply.github.com>
2025-06-29 23:15:27 -0500

    .

--- sga_fisi/settings.py
+++ sga_fisi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7l4gb)@pep8(9g#zfmq9+yo^@6(b7bgi-5$x%vl#n)8-54da=a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -144,17 +144,22 @@ DATABASES = {
     }
 }
 """
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sga_fisi',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sga_fisi',
+#         'USER': 'root',
+#         'PASSWORD': '1234',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+# }
 
 
 # Password validation
